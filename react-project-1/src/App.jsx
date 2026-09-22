01d410ac2e426ab87d529d9816136aacd43b9fc2
Umaima Zainab <umaimazainab429@gmail.com>
2026-09-22 19:32:36 +0500

    Added loop practice questions

---
+++ react-project-1/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar1 from './components/Navbar1'
+import Navbar2 from './components/Navbar2'
+import Navbar3 from './components/Navbar3'
+const App = () => {
+  return (
+    <div>
+<Navbar1 />
+<Navbar2 />
+<Navbar3 />
+
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+
+  )
+}
+
+export default App
